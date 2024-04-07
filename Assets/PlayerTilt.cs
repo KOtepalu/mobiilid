db8ece79ab5175c3d212ce02ec467c84a3361071
Merette Arula <114983020+merettearula@users.noreply.github.com>
2024-04-07 15:05:49 +0300

    debug reziimis koordinaadid

--- Assets/PlayerTilt.cs
+++ Assets/PlayerTilt.cs
@@ -7,25 +7,29 @@ public class PlayerTilt : MonoBehaviour
 {
     public float tiltSpeed;
 
-    // Update is called once per frame
     void Update()
     {
         float tiltInput = Input.acceleration.x;
+        float tiltYInput = Input.acceleration.y;
 
         // Kui kallutamise sisend puudub, jääb mängija paigale
-        if (Mathf.Abs(tiltInput) < 0.1f) // Võite muuta seda väärtust vastavalt soovitud tundlikkusele
+        if (Mathf.Abs(tiltInput) < 0.1f) 
         {
-            return; // Väljub Update meetodist ja jätab mängija paigale
+            return; // Väljub Update meetodist ja jätab samal hetkel mängija paigale
         }
 
-        // Mängija liigutamine vastavalt kallutamisele
+        // Liigutame playerit vastavalt kallutamisele
         Vector2 movement = new Vector2(tiltInput, 0f);
         transform.Translate(movement * tiltSpeed * Time.deltaTime);
+
+        // Kuvame x ja y koordinaadid konsoolile
+        Debug.Log("X: " + tiltInput + ", Y: " + tiltYInput);
+
     }
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //If collides with the player it will reload the block
+        //Kui põrkab kokku blockiga, siis peab uuesti laadima selle scene et mäng uuesti hakkaks
         if(collision.gameObject.tag == "block")
         {
             SceneManager.LoadScene("GameTilt");
