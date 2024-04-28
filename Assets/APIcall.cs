b057a1821968c72423b1a631a59e501fd13d390e
Merette Arula <114983020+merettearula@users.noreply.github.com>
2024-04-28 20:04:32 +0300

    serverist info võtmine töötab

--- Assets/APIcall.cs
+++ Assets/APIcall.cs
@@ -1,57 +1,70 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.Networking;
-using System;
 
 public class APIcall : MonoBehaviour
 {
-    [Serializable]
-    public class UserData
+  [Serializable]
+  public class APIResponse
+  {
+    public UserData data;
+    public SupportData support;
+  }
+
+  [Serializable]
+  public class UserData
+  {
+    public int id;
+    public string email;
+    public string first_name;
+    public string last_name;
+    public string avatar;
+  }
+
+  [Serializable]
+  public class SupportData
+  {
+    public string url;
+    public string text;
+  }
+
+  public TextMeshProUGUI text;
+
+  void Start()
+  {
+    RefreshData();
+  }
+
+  public void OnRefresh()
+  {
+    RefreshData();
+  }
+
+  void RefreshData()
+  {
+    StartCoroutine(GetRequest("https://reqres.in/api/users/2"));
+  }
+
+  IEnumerator GetRequest(string uri)
+  {
+    using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
     {
-        public string first_name;
+      yield return webRequest.SendWebRequest();
+
+      if (webRequest.result == UnityWebRequest.Result.ConnectionError ||
+          webRequest.result == UnityWebRequest.Result.DataProcessingError)
+      {
+        Debug.LogError($"Error: {webRequest.error}");
+      }
+      else if (webRequest.result == UnityWebRequest.Result.Success)
+      {
+        string jsonResponse = webRequest.downloadHandler.text;
+        APIResponse responseData = JsonUtility.FromJson<APIcall.APIResponse>(jsonResponse);
+        UserData userData = responseData.data;
+        text.text = userData.first_name;
+      }
     }
-
-    public TextMeshProUGUI text;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Call the API when the script starts
-        RefreshData();
-    }
-
-    public void OnRefresh ()
-    {
-        // Call the API when the button is clicked
-        RefreshData();
-    }
-
-    void RefreshData()
-    {
-        StartCoroutine(GetRequest("https://reqres.in/api/users/2"));
-    }
-
-    IEnumerator GetRequest(string uri)
-    {
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
-        {
-            yield return webRequest.SendWebRequest();
-
-            if (webRequest.result == UnityWebRequest.Result.ConnectionError || 
-                webRequest.result == UnityWebRequest.Result.DataProcessingError)
-            {
-                Debug.LogError($"Error: {webRequest.error}");
-            }
-            else if (webRequest.result == UnityWebRequest.Result.Success)
-            {
-                string jsonResponse = webRequest.downloadHandler.text;
-                UserData userData = JsonUtility.FromJson<UserData>(jsonResponse);
-                text.text = userData.first_name;
-            }
-        }
-    }
+  }
 }
-
-
